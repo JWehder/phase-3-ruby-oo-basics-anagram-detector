e3cedad645901c332838078369222b0c1e5cadcd
Jake Wehder <jw@email.comjake>
2022-11-27 16:37:35 -0600

    commit tree 93e425ec83ba0c84edaa92819164f8c0e4dc397e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        # create an array of letters
+        # create a loop to compare each word in the array with the word
+        # create a conditional to end loop once match has been found
+        # if no match has been found, then return an empty array
+        @letters = @word.chars
+        array.filter do | word |
+            array_word_letters = word.chars
+            if array_word_letters.sort == @letters.sort
+                puts word
+                true
+            else 
+                false
+            end
+        end
+    end
+
+end
+
+listen = Anagram.new("listen")
+matcher = listen.match(%w[enlists google inlets banana])
+
